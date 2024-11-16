bd3fdfc9f35333aafe6f2431914021e06e069984
Alex <alexsandrodp1@gmail.com>
2024-11-16 17:10:43 -0300

    lists

---
+++ src/main/java/com/alexsandro/dslist/service/GameListService.java
@@ -0,0 +1,26 @@
+package com.alexsandro.dslist.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.alexsandro.dslist.dto.GameListDTO;
+import com.alexsandro.dslist.entities.GameList;
+import com.alexsandro.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+	
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+		
+	}
+
+}
